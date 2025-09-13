5a8059202b3ba1f4b0eef2c2e1b70a3028060586
IceEz <apiwit806@gmail.com>
2025-09-13 12:56:21 +0700

    FixCode follow the instruction

--- yolo_detector.py
+++ yolo_detector.py
@@ -1,10 +1,15 @@
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
-import cv2
+import cv2 as cv
+from collections import defaultdict
+import numpy as np
 
 # Load YOLO model
-model = YOLO("yolov8a.pt")
+model = YOLO("yolov8n.pt")
+print(model.names)
 
+# Store the track history
+track_history = defaultdict(lambda: [])
 
 def draw_boxes(frame, boxes):
     """Draw detected bounding boxes on image frame"""
@@ -16,12 +21,10 @@ def draw_boxes(frame, boxes):
         class_name = model.names[int(class_id)]
         coordinator = box.xyxy[0]
         confidence = box.conf
-
     # Draw bounding box
-    annotator.box_label(
-        box=coordinator, label=class_name, color=colors(class_id, True)
+        annotator.box_label(
+        box=coordinator, label=class_name, color=colors(18, False)
     )
-
     return annotator.result()
 
 
@@ -29,11 +32,12 @@ def detect_object(frame):
     """Detect object from image frame"""
 
     # Detect object from image frame
-    results = model.prediction(frame)
+    results = model.predict(frame, classes=[15])
+    
 
     for result in results:
-    frame = draw_boxes(frame, result.boxes)
-
+        frame = draw_boxes(frame, result.boxes)
+    
     return frame
 
 
@@ -41,23 +45,44 @@ if __name__ == "__main__":
     video_path = "CatZoomies.mp4"
     cap = cv.VideoCapture(video_path)
 
+
+
     # Define the codec and create VideoWriter object
-    video_writer = cv.VideoWriter(
-        video_path + "_demo.avi", cv.VideoWriter_fourcc(*"MJPG"), 30, (1280, 720)
-    )
+    # video_writer = cv.VideoWriter(
+    #     video_path + "_demo.avi", cv.VideoWriter_fourcc(*"MJPG"), 30, (1280, 720)
+    # )
 
     while cap.isOpened():
         # Read image frame
-        ret, frame = cap.read_frame()
+        ret, frame = cap.read()
+    
+
+        ## Line tracking Use Plotting Tracks Over Time From Ultralytics Docs
+        result = model.track(frame, persist=True)[0]
+        if result.boxes and result.boxes.is_track:
+            boxes = result.boxes.xywh.cpu()
+            track_ids = result.boxes.id.int().cpu().tolist()
+            # Plot the tracks
+            for box, track_id in zip(boxes, track_ids):
+                x, y, w, h = box
+                track = track_history[track_id]
+                track.append((float(x), float(y)))  # x, y center point
+                if len(track) > 30:  # retain 30 tracks for 30 frames
+                    track.pop(0)
+                # Draw the tracking lines
+                points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                cv.polylines(frame, [points], isClosed=False, color=(230, 230, 230), thickness=5)
+
 
         if ret:
             # Detect motorcycle from image frame
             frame_result = detect_object(frame)
 
             # Write result to video
-            video_writer.write(frame_result)
+            #  video_writer.write(frame_result)
 
             # Show result
+            cv.putText(frame_result,"Apivit-Clicknext-Internship-2025",(700,50),cv.FONT_HERSHEY_SIMPLEX,1,(	(0, 0, 255)),2)
             cv.namedWindow("Video", cv.WINDOW_NORMAL)
             cv.imshow("Video", frame_result)
             cv.waitKey(30)
@@ -66,6 +91,10 @@ if __name__ == "__main__":
             break
 
     # Release the VideoCapture object and close the window
-    video_writer.release()
+    # video_writer.release()
     cap.release()
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
+
+
+
+
